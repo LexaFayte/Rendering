28121452202b361f47248a8d07fd8ce119d2baf6
Alexandra Kabak <luciferfayte@hotmail.com>
2018-01-28 12:37:37 -0800

    completed Lesson 3: rendering frames

--- DX11Tutorial/DX11Tutorial/main.cpp
+++ DX11Tutorial/DX11Tutorial/main.cpp
@@ -1,5 +1,26 @@
 #include <windows.h>
 #include <windowsx.h>
+#include <d3d11.h>
+#include <d3dx11.h>
+#include <d3dx10.h>
+#include <d3dcompiler.h>
+
+//include the Direct3D Library file
+#pragma comment (lib, "d3d11.lib")
+#pragma comment (lib, "d3dx11.lib")
+#pragma comment (lib, "d3dx10.lib")
+
+//globals
+IDXGISwapChain *swapchain;
+ID3D11Device *dev;
+ID3D11DeviceContext *devcon;
+ID3D11RenderTargetView *backbuffer;
+
+//prototypes
+void InitD3D(HWND hWnd);
+void RenderFrame(void);
+void CleanD3D(void);
+
 
 /*
 	prototype for WndProc
@@ -23,7 +44,6 @@ int WINAPI WinMain(
 	HWND hWnd;
 	WNDCLASSEX wc;
 
-	//clear out the window class for use (initializes a block of memory of size WNDCLASSEX to NULL)
 	ZeroMemory(&wc, sizeof(WNDCLASSEX));
 
 	wc.cbSize = sizeof(WNDCLASSEX);
@@ -32,22 +52,21 @@ int WINAPI WinMain(
 	wc.hInstance = hInstance;
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
-	wc.lpszClassName = LPCSTR("WindowClass1");
+	wc.lpszClassName = (LPCSTR)"WindowClass";
 
 	RegisterClassEx(&wc);
 
-	RECT wr = { 0,0,500,500 }; //sets the size, but not the position
+	RECT wr = { 0, 0, 800, 600 };
 	AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);
 
-	hWnd = CreateWindowEx(
-		NULL,
-		LPCSTR("WindowClass1"),
-		LPCSTR("DX11 Window!"),
+	hWnd = CreateWindowEx(NULL,
+		(LPCSTR)"WindowClass",
+		(LPCSTR)"Our First Direct3D Program",
 		WS_OVERLAPPEDWINDOW,
-		300,//x pos
-		300,//y pos
-		wr.right - wr.left,//width
-		wr.bottom - wr.top,//height
+		300,
+		300,
+		wr.right - wr.left,
+		wr.bottom - wr.top,
 		NULL,
 		NULL,
 		hInstance,
@@ -55,10 +74,13 @@ int WINAPI WinMain(
 
 	ShowWindow(hWnd, nShowCmd);
 
+	//initialize D3D
+	InitD3D(hWnd);
+
 	//MSG is a struct that holds Windows events messages
 	MSG msg;
 
-	while (true)
+	while (TRUE)
 	{
 		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
 		{
@@ -71,9 +93,10 @@ int WINAPI WinMain(
 		else
 		{
 			//run game code...
-		}
+			RenderFrame();
+		}	
 	}
-
+	CleanD3D();
 	return msg.wParam;
 }
 
@@ -94,3 +117,86 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
 
 	return DefWindowProc(hWnd, message, wParam, lParam);
 }
+
+/*
+	DirectX 11 setup functions
+*/
+
+void InitD3D(HWND hWnd)
+{
+	//swap chain description struct
+	DXGI_SWAP_CHAIN_DESC scd;
+
+	//clear out structs memory to zero
+	ZeroMemory(&scd, sizeof(scd));
+
+	//fill out the swap chain description struct
+	scd.BufferCount = 1; //one back buffer; could have more
+	scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;      // how swap chain is to be used
+	scd.OutputWindow = hWnd;
+	scd.SampleDesc.Count = 1; //MSAA
+	scd.SampleDesc.Quality = 0;
+	scd.Windowed = TRUE;
+
+	//create the device, device context, and swap chain
+	HRESULT error;
+	error = D3D11CreateDeviceAndSwapChain(NULL,
+		D3D_DRIVER_TYPE_HARDWARE,
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		D3D11_SDK_VERSION,
+		&scd,
+		&swapchain,
+		&dev,
+		NULL,
+		&devcon);
+
+	//get address of backbuffer
+	ID3D11Texture2D *pBackBuffer;
+	swapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+
+	//use the backbuffer address to create the render target
+	dev->CreateRenderTargetView(pBackBuffer, NULL, &backbuffer);
+	pBackBuffer->Release();
+
+	//set the render target as the back buffer
+	devcon->OMSetRenderTargets(1, &backbuffer, NULL);
+
+	//set the viewport
+	D3D11_VIEWPORT viewport;
+	ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
+
+	viewport.TopLeftX = 0;
+	viewport.TopLeftY = 0;
+	viewport.Width = 800;
+	viewport.Height = 600;
+
+	devcon->RSSetViewports(1, &viewport);
+
+
+}
+
+void RenderFrame(void)
+{
+	//clear the backbuffer to a colour
+	devcon->ClearRenderTargetView(backbuffer, D3DXCOLOR(0.45f, 0.2f, 0.4f, 1.0f));
+
+	//do 3D rendering on the backbuffer
+
+
+	//switch the backbuffer and the frontbuffer
+	swapchain->Present(0, 0);
+}
+
+void CleanD3D(void)
+{
+	//release all COM objects
+	swapchain->Release();
+	backbuffer->Release();
+	dev->Release();
+	devcon->Release();
+}
+
